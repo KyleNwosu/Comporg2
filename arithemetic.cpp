ea1b2832edc6a15a3bd51e6d5268903cafccd2c7
Komplete101 <kylenwosu2003@gmail.com>
2024-03-29 00:12:52 -0400

    Finished timing: arithemetic.cpp

--- arithemetic.cpp
+++ arithemetic.cpp
@@ -14,22 +14,35 @@ void Benchmark(){
         result = 1 + 1;
     }
 
+
     for (int i = 0; i < 5; i++){
         for (int j = 0; j < pow(10,9); j++){
             result = 1 * 1;
         }
     }
 
+
     for (int i =0; i < 2; i++){
         for (int j = 0; j < pow(10,9); j++){
             result = 1/1;
         }
     }
+
     cout << "Finished";
 
 }
 
 int main(){
+
+    std:: chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
     Benchmark();
+    end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+    cout << "Finished computation at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << "s\n";
+
     return 0;
 }
