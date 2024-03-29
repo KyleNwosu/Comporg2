7b52f7f75c107fd036068a559149a6cfd561ffd2
Komplete101 <kylenwosu2003@gmail.com>
2024-03-29 14:16:33 -0400

    Finished the arithemetic program

--- arithemetic.cpp
+++ arithemetic.cpp
@@ -10,23 +10,25 @@ using namespace std:: chrono;
 
 void Benchmark(){
     int result;
+
+    cout << "Starting";
     for (int i = 0; i< pow(10,10); i++){
         result = 1 + 1;
     }
 
 
-    // for (int i = 0; i < 5; i++){
-    //     for (int j = 0; j < pow(10,9); j++){
-    //         result = 1 * 1;
-    //     }
-    // }
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < pow(10,9); j++){
+            result = 1 * 1;
+        }
+    }
 
 
-    // for (int i =0; i < 2; i++){
-    //     for (int j = 0; j < pow(10,9); j++){
-    //         result = 1/1;
-    //     }
-    // }
+    for (int i =0; i < 2; i++){
+        for (int j = 0; j < pow(10,9); j++){
+            result = 1/1;
+        }
+    }
 
     cout << "Finished";
 
