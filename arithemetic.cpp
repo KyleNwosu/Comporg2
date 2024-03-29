0404f734d8fa2959165ba8b3d4dab1ce7b0d8f7b
Komplete101 <106222631+Komplete101@users.noreply.github.com>
2024-03-28 22:24:56 -0400

    New changes

--- arithemetic.cpp
+++ arithemetic.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
-
+#include <cmath> // Include the <cmath> header for the pow() function
 using namespace std;
 
 int main(){
-    cout << "Hello World!";
+    int a = 1;
+    int b = 1; // Declare the variable 'b'
+    for (int i =0; i < pow(10,10); i++){
+        int sum = a + b;
+    }
     return 0;
 }
